1daf8db000caf00b2f96e10f4ea7a8bcd1a5923d
Christoffer chk. Krook <chk@lasers.kth.se>
2019-12-01 11:26:39 +0100

    .
    .

--- day1.m
+++ day1.m
@@ -12,18 +12,24 @@ weight = accumulated_weight
 
 % part 2
 accumulated_weight = 0;
+% variabler för att hålla div info. Temp har värde för att loopen inte ska
+% stoppa direkt. Måste initieras med ett värde
 temp = 1000000;
 fuel_per_module = 0;
 for i=1:1:length(data)
+    %vanliga för modulen
     current_module = (floor(data(i)/3)-2);
+    %startvärde för bränslets vikt
     temp = (floor(current_module/3)-2);
     fuel_per_module = temp;
+    %iterera för helvete
     while temp >= 0
         temp = (floor(temp/3)-2);
         if temp > 0
             fuel_per_module = fuel_per_module + temp;
         end
     end
+    %boom
     accumulated_weight = accumulated_weight + current_module + fuel_per_module;
 end
 
