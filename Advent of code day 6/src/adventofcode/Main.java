d11c566674625527d2c92070ab5b45dd8eaaad15
krookc <44060831+krookc@users.noreply.github.com>
2019-12-06 10:29:50 +0100

    day6part1
    day6part1

--- Advent of code day 6/src/adventofcode/Main.java
+++ Advent of code day 6/src/adventofcode/Main.java
@@ -1,8 +1,96 @@
 package adventofcode;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
-	
-	public static void main(String args[]) {
-		
+
+	public List<Planet> planets = new ArrayList<Planet>();
+	public int indirectroutes = 0;
+
+
+	public static void main(String args[]) throws IOException{
+		Main main = new Main();
+	}
+
+	public Main () throws IOException {
+		File file = new File("C:\\Users\\chris\\Documents\\FROG-XFROG\\Advent-of-Code\\Advent of code day 6\\src\\adventofcode\\day6input.txt");
+
+		BufferedReader br = new BufferedReader(new FileReader(file));
+
+		String line;
+		while ((line = br.readLine()) != null) {
+			String[] p = line.split(" ");
+			String p1 = p[0];
+			String p2 = p[1];
+			Planet orbitee = null;
+			Planet orbiter = null;
+			if (planets.size() != 0) {
+				for (Planet pla : planets) {
+					if(pla.name.equals(p1)){
+						orbitee = pla;
+					}else if(pla.name.equals(p2)){
+						orbiter = pla;
+					}
+				}
+				if (orbitee == null) {
+					orbitee = new Planet(p1);
+					planets.add(orbitee);
+				}
+				if (orbiter == null) {
+					orbiter = new Planet(p2);
+					planets.add(orbiter);
+				}
+			}else {
+				orbitee = new Planet(p1);
+				orbiter = new Planet(p2);
+				planets.add(orbitee);
+				planets.add(orbiter);
+			}
+
+			if(orbitee.hasChild(orbiter) != true) {
+				orbitee.addChild(orbiter);
+			}
+		}
+
+		int index = 0;
+
+		for (Planet p : planets) {
+			if (p.parent == null){
+				indirectroutes = this.getIndirect(p);
+			}
+		}
+
+		System.out.println(this.getDirect()+this.indirectroutes);
+	}
+
+	public int getDirect() {
+		return planets.size()-1;
+	}
+
+	public int getIndirect(Planet p) {
+		int index = 0;
+
+		int currentIt = 0;
+
+		for (Planet current : planets) {
+			Planet temp = current;
+			while (temp.getParent() != null) {
+				if (currentIt == 0) {
+					temp = temp.getParent();
+					currentIt = 1;
+				}else {
+					index+=1;
+					temp = temp.getParent();
+				}
+			}
+			currentIt = 0;
+		}
+		return index;
 	}
 }
--- Advent of code day 6/src/adventofcode/Planet.java
+++ Advent of code day 6/src/adventofcode/Planet.java
@@ -1,8 +1,12 @@
 package adventofcode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Planet {
 	
 	public Planet parent = null;
+	public List<Planet> children = new ArrayList<Planet>();
 	public String name = "";
 	
 	public Planet(Planet planet, String name) {
@@ -11,5 +15,23 @@ public class Planet {
 	
 	public Planet(String name) {
 		this.name = name;
+	}
+	
+	public boolean hasChild(Planet pla) {
+		for (Planet c : children) {
+			if (c.name.equals(pla.name)) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public void addChild(Planet a) {
+		children.add(a);
+		a.parent = this;
+	}
+	
+	public Planet getParent() {
+		return this.parent;
 	}
 }
